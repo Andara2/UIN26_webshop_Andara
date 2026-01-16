b149d07de1a4f9da296028e12d1987225218859d
Andara2 <maandara@hiof.no>
2026-01-16 13:55:40 +0100

    1

--- LEGOdudes/shopscript.js
+++ LEGOdudes/shopscript.js
@@ -18,10 +18,63 @@ function fetchProductInfo() {
 
 fetchProductInfo()
 
+//Generer handlevogn visning:
+function showCart() {
+    //Unike produkter
+    let uniqueItems = new Set(cart)
+    let uniqueArray = [...uniqueItems]
+    //Oversikt over antall per produkt
+    let CartItems = []
+    uniqueArray.map(item => {
+        cart.filter(i => i === item).length
+        CartItems.push({prodid: item, quantity: cart.filter (i => i === item).length})
+    })
+    
+    //Gå gjennom cartItems for å lage HTML til handlekurv og regne ut totalpris
+    let cartHTML = ""
+    let totalPrice = 0
+    
+    CartItems.map(ci => {
+        //Hente produktinformasjon
+        let product = products.find(i => i.prodid === ci.prodid)
+        //Skrive ut HTML
+        cartHTML += `<tr>
+                    <td class="title">${product.title}</td>
+                    <td class="price">${product.price}</td>
+                    <td class="quantity">${ci.quantity}</td>
+                    <td class="delete"><butten onClick="deleteFromCart(${product.prodid})">X</button></td>
+                </tr>`
+        //Summere totalpris
+        totalPrice += Number(product.price) * ci.quantity
+    })
+
+    if(cart.length === 0) {
+        cartHTML += "<tr><td>Ingen varer</td></tr>"
+    }
+
+    //Oppdater HTML-elementer
+    document.getElementById("cart-items").innerHTML = cartHTML
+    document.getElementById("total-price").innerHTML = totalPrice
+    document.getElementById("cart-quantity").innerText = cart.length
+}
+
+//Slette fra handlekurv
+function deleteFromCart(prodid) {
+    let deleteIndex = cart.indexOf(prodid)
+    if(deleteIndex >-1) {
+        cart.splice(deleteIndex, 1)
+    }
+    //Oppdatere handlevogn-utskrift
+    showCart()
+}
+
 //Legg til handlevogn funksjon:
 function addToCart(prodid) {
     console.log("Legg til produkt med id: " + prodid)
     cart.push(prodid)
     console.log(cart)
     document.getElementById("cart-quantity").innerText = cart.length
+
+    //oppdatere handlevongvisning
+    showCart()
 }
